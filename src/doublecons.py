added ligand average for evolutionary score, also fixed blosum score 0-1 and jsdw score from capra's python code

src/doublecons.py
@@ -10,6 +10,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sb
 
+def make_blos62_mp():
+    # blosum 62 marginal probabilities, from capra python code
+    sc = [0.078, 0.051, 0.041, 0.052, 0.024, 0.034, 0.059, 0.083, 0.025, 0.062, 0.092, 0.056, 0.024, 0.044, 0.043,
+          0.059, 0.055, 0.014, 0.034, 0.072]
+    aa = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
+    return {aa[i]: sc[i] for i in range(len(sc))}
+
 class ConScores:
     """
     class containing conservation scores.
@@ -20,27 +27,30 @@ class ConScores:
 
     # general variab
 
-    # blosum 62 marginal probabilities
-    blos62_mp = {'A': 0.074,
-                 'C': 0.025,
-                 'D': 0.054,
-                 'E': 0.054,
-                 'F': 0.047,
-                 'G': 0.074,
-                 'H': 0.026,
-                 'I': 0.068,
-                 'K': 0.058,
-                 'L': 0.099,
-                 'M': 0.025,
-                 'N': 0.045,
-                 'P': 0.039,
-                 'Q': 0.034,
-                 'R': 0.052,
-                 'S': 0.057,
-                 'T': 0.051,
-                 'V': 0.073,
-                 'W': 0.013,
-                 'Y': 0.032}
+    blos62_mp = make_blos62_mp()
+
+
+    #
+    # old_blos62_mp = {'A': 0.074,
+    #              'C': 0.025,
+    #              'D': 0.054,
+    #              'E': 0.054,
+    #              'F': 0.047,
+    #              'G': 0.074,
+    #              'H': 0.026,
+    #              'I': 0.068,
+    #              'K': 0.058,
+    #              'L': 0.099,
+    #              'M': 0.025,
+    #              'N': 0.045,
+    #              'P': 0.039,
+    #              'Q': 0.034,
+    #              'R': 0.052,
+    #              'S': 0.057,
+    #              'T': 0.051,
+    #              'V': 0.073,
+    #              'W': 0.013,
+    #              'Y': 0.032}
 
     # scoring methods
 
@@ -70,8 +80,45 @@ class ConScores:
 
         return out
 
+    # this is the new blosum score, normalized by min and max in the matrix
     @staticmethod
     def blosum(msa, ref):
+        """
+        blosum score: sum of BLOSUM(ref, L) for L in other ligands.
+        Normalized by the number of sums and min/max blos62 score
+        :param msa:
+        :param ref:
+        :return:
+        """
+        Mb,mb = max(blosum62.values()), min(blosum62.values())
+
+        out = []
+        reflen = len(msa.loc[ref])
+
+        for i in range(reflen):
+            norm = 0
+            res_list = list(msa.iloc[:, i])
+            reference_res = str(msa.iloc[:, i].loc[ref])
+
+            if reference_res not in "-.":
+                this_score = - blosum62[(reference_res, reference_res)]  # starts in debt to cover self reference hit
+                for res in res_list:
+                    if res not in "-.":
+                        norm += 1
+                        # exception to catch stupid triangular matrix
+                        try:
+                            bloscore = blosum62[(reference_res, res)]
+                        except KeyError:
+                            bloscore = blosum62[(res, reference_res)]
+                        this_score += bloscore
+                # print(", ".join([reference_res, "\t"] + res_list + [str(round(this_freq,2))]))
+                out.append(np.round(((this_score / norm) - mb )/(Mb-mb), 1))
+
+        return out
+
+    # this is the old blosum score, kept for reference
+    @staticmethod
+    def old_blosum(msa, ref):
         """
         blosum score: sum of BLOSUM(ref, L) for L in other ligands.
         Normalized by the number of sums
@@ -109,7 +156,7 @@ class ConScores:
         """
         Jensen shannon divergence with window of residues.
         as in https://academic.oup.com/bioinformatics/article/23/15/1875/203579
-        used by consurf!
+        used by consurf! fake!
 
         RE = sum aa Pc(aa) * log (Pc(aa) / q(aa))
 
@@ -168,8 +215,12 @@ class ConScores:
 
             # compute q and r dictionaries
             q = ConScores.blos62_mp  # dict of "aa" -> blos62 frequency
-            r = {x: (lamb*Pc[x]) + ((1-lamb)*q[x]) for x in Pc}  # dict of "aa" -> r score
+            # add zeroes in Pc
+            for aa in q:
+                if aa not in Pc:
+                    Pc[aa] = 0.0
 
+            r = {x: (lamb*Pc[x]) + ((1-lamb)*q[x]) for x in Pc}  # dict of "aa" -> r score
             # add aa to r and Pc if not present with (almost) zero
             # for k in q:
             #     if k not in r:
@@ -177,28 +228,34 @@ class ConScores:
             #     if k not in Pc:
             #         Pc[k] = 10**-6
 
+            # modified
             # RE score with case for zero division inside log (it tends to 1)
-            def RE(d1, d2):
-                o = []
-                for x in set(d1.keys()) & set(d2.keys()):
-                    try:
-                        v = d1[x]*(np.log(d1[x]/d2[x]))
-                    except ZeroDivisionError:
-                        v = d1[x]
-                    o.append(v)
-                return sum(o)
+            # def RE(d1, d2):
+            #     d = 0
+            #     for x in set(d1.keys()) & set(d2.keys()):
+            #         try:
+            #             v = d1[x]*(np.log(d1[x]/d2[x]))
+            #         except ZeroDivisionError:
+            #             v = d1[x]
+            #         o.append(v)
+            #     return sum(o)
+            d = 0
+            for aa in Pc:
+                if r[aa] != 0.0:
+                    if Pc[aa] == 0.0:
+                        d += q[aa] * np.log2(q[aa] / r[aa])
+                    elif q[aa] == 0.0:
+                        d += Pc[aa] * np.log2(Pc[aa] / r[aa])
+                    else:
+                        d += Pc[aa] * np.log2(Pc[aa] / r[aa]) + q[aa] * np.log2(q[aa] / r[aa])
 
-            # print(Pc)
-            # print(sum(Pc.values()))
-            # print(q)
-            # print(sum(q.values()))
-            # print(r)
-            # print(sum(r.values()))
-            # finally compute score
-            DC = (lamb * RE(Pc, r)) + ((1 - lamb) * RE(q, r))
+            # d /= 2 * math.log(len(fc))
+            d /= 2
+
             # add gap penalty
-            DC = DC * (1 - gap_pen)
-            return DC
+            #check here for gap penalty
+            d = d * (1 - gap_pen)
+            return d
 
         #######################
 
@@ -972,7 +1029,6 @@ def inter_coevol(p1, p2, outpath, test):
             MI = mutual_info(test)
             MI = round(MI, 2)
             miatrix[i][j] = MI
-            miatrix[j][i] = MI
 
     if test == "MIp":
         miatrix = correct_apc(miatrix)

src/run_double_conservation.py
@@ -142,9 +142,132 @@ def con_con_comparison(args):
     with open("{}/{}concon.csv".format(args.output_path, prot.name), "w") as outfile:
         cs.to_csv(outfile, sep="\t")
 
+def dir_prediction(args):
+    """
+    Divergence Inducing Residue Prediction
+    Basically like cross-conservation but averaging over all paralogs and correcting for coevolution
+
+    :param args:
+    :return:
+    """
+    rp = args.concon_reference          # reference ligand name
+    prot = args.ligands[rp]             # reference ligand
+    seq = prot.get_ref_seq()            # reference ligand sequence
+    msta = args.msta                    # multiple structure alignment
+    msa = args.msa                      # multiple sequence alignment
+    contest = args.conservation_test    # conservation test type
+    algtest = args.alignment_test       # conservation for ligand alignment type
+
+    # data preparation
+    cs = pd.DataFrame()
+    # add separating variable when it is 7 lumps of data
+    sepvar = 0
+    if algtest == "id":
+        sepvar = 0.04
+    cs["MSA"] = [round(x, 2) + sepvar for x in msa.get_cons_scores(algtest)]
+    cs["MSTA"] = [round(x, 2) - sepvar for x in msta.get_cons_scores(algtest)]
+    relative_conservation = ligand_table(args)
+    # cs[prot.name] = prot.get_cons_scores(contest)
+    # now I need one evo cons from MSA alignment average and one from MSTA alignment average
+    cs["EVO cons MSA"] = average_cons(relative_conservation,"MSA")
+    cs["EVO cons MSTA"] = average_cons(relative_conservation,"MSTA")
+    cs.insert(0, 'POS', [str(x) + seq[x - 1] for x in range(1, 1 + len(cs))])
+
+    # plotting
+    fig, ax = plt.subplots()
+    fig.set_size_inches(11.7, 8.27)  # A4 size
+
+    def plotter(fig, ax, alg_label, marker, pname):
+
+        # colormap
+        cmap = matplotlib.cm.get_cmap('coolwarm')
+        normalize = matplotlib.colors.Normalize(vmin=0, vmax=len(cs[alg_label]))
+        colors = [cmap(normalize(value)) for value in range(len(cs[alg_label]))]
+
+        #scatter plot
+        sb.regplot(alg_label, pname, cs, scatter=True, fit_reg=False, ax=ax, label=alg_label, marker=marker, scatter_kws={"color":colors})
+        # labels
+        dpos = set()
+
+        def label_point(x, y, val, ax, dpos):
+            a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
+            for i, point in a.iterrows():
+                # point['x'] += 0.01
+                # i = 0
+                # while (point['x'], point['y']) in dpos:
+                #     if i == 1:
+                #         point['x'] -= 0.04
+                #         point['y'] -= 0.01
+                #         i = -1
+                #     if i == 1 and round(point['x'], 2) == 1:
+                #         point['x'] -= 0.04
+                #         point['y'] -= 0.01
+                #         i = -1
+                #     i += 1
+                #     point['x'] += 0.02
+                dpos.add((point['x'], point['y']))
+                ax.text(point['x'], point['y'], str(point['val']), fontsize=5)
+
+        label_point(cs[alg_label], cs["EVO cons {}".format(alg_label)], cs.POS, plt.gca(), dpos)
+        label_point(cs[alg_label], cs["EVO cons {}".format(alg_label)], cs.POS, plt.gca(), dpos)
+
+    plotter(fig, ax, "MSA", 'd', "EVO cons MSA")
+    plotter(fig, ax, "MSTA", 's', "EVO cons MSTA")
+
+    # labels
+    plt.ylabel("Average Evolutionary Conservation")
+    plt.xlabel("Ligands Alignments Conservation")
+    plt.legend(loc=4)
+
+    # # colorbar
+    cmap = matplotlib.cm.get_cmap('coolwarm')
+    normalize = matplotlib.colors.Normalize(vmin=0, vmax=len(cs["MSA"]))
+    cax, _ = matplotlib.colorbar.make_axes(ax, orientation="horizontal", aspect=50)
+    cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=normalize, orientation="horizontal")
+    plt.xlabel("AA position")
+
+    # plot diagonal line
+    lims = [
+        np.min([ax.get_xlim(), ax.get_ylim()]),
+        np.max([ax.get_xlim(), ax.get_ylim()]),
+        ]
+    ax.plot(lims, lims, ls="--", c=".3")
+    # in case it is 7 lumps of X axis
+    if algtest == "id":  # it is 7 blocks
+        ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))  # float representation to 2 decimals
+        ax.bar(np.arange(0, 1.01, 1 / 6), [ax.get_ylim()[1]] * 7, width=[0.15] * 7, color="grey", alpha=0.3)  # backgroun bars
+        plt.xticks(np.arange(0, 1.01, 1 / 6))  # ligands xticks
+
+    # saving
+    plt.savefig("{}/{}dirp.png".format(args.output_path, prot.name), format='png', dpi=800)
+
+    # save text output also but add average
+    # average MSA/MSTA then distance with
+    cs["avg DIR score"] = calculate_dirp_score(cs,["MSA","EVO cons MSA"],["MSTA","EVO cons MSTA"])
+    with open("{}/{}dirp.csv".format(args.output_path, prot.name), "w") as outfile:
+        cs.to_csv(outfile, sep="\t")
+
+def calculate_dirp_score(df, msa_id=["MSA","EVO cons MSA"], msta_id=["MSTA","EVO cons MSTA"]):
+    """average msa and msta score (distance from diagonal)"""
+
+    diag = lambda x,y: np.abs(x-y) / np.sqrt(2)
+    MSA_score = diag(df[msa_id[0]],df[msa_id[1]])
+    MSTA_score = diag(df[msta_id[0]],df[msta_id[1]])
+    return np.mean([MSA_score,MSTA_score], axis=0)
+
+
+def average_cons(d,alg_type):
+    """returns the average conservation from the dictionary of relative scores"""
+    l = []
+    for k in d:
+        if alg_type in k:
+            a = [np.nan if x == "-" else float(x) for x in d[k]]
+            l.append(a)
+    mean_out = np.nanmean(l,axis=0)
+    return mean_out
+
 def ligand_table(args):
     """
-
     :param args:
     :return:
     """
@@ -183,6 +306,7 @@ def ligand_table(args):
     with open("{}/ligand_table.csv".format(args.output_path), "w") as outfile:
         cs.to_csv(outfile, sep="\t")
 
+    return rel_cons
 
 def coevol_test(args):
 
@@ -344,6 +468,7 @@ def main(argv):
     parser.add_argument("--output-path", required=True, help="path to output folder, will be created if not ex")
     parser.add_argument("--test-type", required=True, help="""The type of test:
      CONCON - cross conservation graph,
+     DIRP - average multiple concon, or Divergence Inducing Residue Prediction,
      LTABLE - ligand positions to reference table
      COEVOL - coevolution test between ligands and receptor
      PCA    - PCA done from the conservation scores of ligand table. needs ligtable as input""")
@@ -383,12 +508,15 @@ def main(argv):
     if args.test_type == "CONCON":
         con_con_comparison(args)
 
-    # LIGAND TABLE
-    if args.test_type == "LTABLE":
-        ligand_table(args)
+    # DIRP
+    if args.test_type == "DIRP":
+        dir_prediction(args)
 
     if args.test_type == "COEVOL":
         coevol_test(args)
+
+    if args.test_type == "LIGTAB":
+        _ = ligand_table(args)
 
     # PCA from ligand table
     if args.test_type == "PCA":